9d2f9b8666b1ec6bec32b7e7652d317b30332883
Cristian <cf.sandovalc@uea.edu.ec>
2026-07-27 16:48:48 -0500

    Configuración para GitHub Pages con base /FR-MOTORS/

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/FR-MOTORS/",   // 👈 clave para GitHub Pages
   plugins: [react()],
   server: {
     port: 5173,
     host: true
   }
 })
-
